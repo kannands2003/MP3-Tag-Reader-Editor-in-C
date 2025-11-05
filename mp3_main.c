d060d4b4ba8dcff32686d412774a102a6418fa9c
kannands2003 <kannands406@gmail.com>
2025-11-05 19:48:12 +0530

    Enhance MP3 tag reader help and error handling
    Updated help message to include additional MP3 tag options and improved error messages.

--- mp3_main.c
+++ mp3_main.c
@@ -1,8 +1,3 @@
-/* 
-    Name: Kannan D S
-	Roll No: ECEP 25021B 065
-    mp3_tag_reader : MP3 tag reader and editor
-*/
 
 #include<stdio.h>
 #include<string.h>
@@ -15,7 +10,7 @@ int main(int argc, char *argv[])
 	if(argc == 2 && strcmp(argv[1], "--help") == 0) // for help option
 	{
 		printf("=============================================================\n");
-        printf("                 MP3 TAG READER AND EDITOR \n");
+        printf("                 MP3 TAG READER AND EDITOR (v1.0)\n");
         printf("=============================================================\n\n");
 
         printf("USAGE:\n");
@@ -25,11 +20,29 @@ int main(int argc, char *argv[])
         printf("TAG OPTIONS:\n");
         printf("  -t  : Title\n");
         printf("  -a  : Artist\n");
+        printf("  -b  : Band\n");
+        printf("  -d  : Conductor\n");
+        printf("  -r  : Remixer\n");
         printf("  -A  : Album\n");
+        printf("  -n  : Track Number\n");
+        printf("  -p  : Disc Number\n");
+        printf("  -g  : Genre\n");
         printf("  -y  : Year\n");
+        printf("  -D  : Date/Time\n");
+        printf("  -l  : Language\n");
         printf("  -c  : Comment\n");
-        printf("  -C  : Music\n");
-		
+        printf("  -x  : Custom Tag\n");
+        printf("  -w  : Lyricist\n");
+        printf("  -m  : Composer\n");
+        printf("  -C  : Copyright\n");
+        printf("  -P  : Publisher\n");
+        printf("  -s  : ISRC Code\n");
+        printf("  -L  : Lyrics\n");
+        printf("  -o  : Original Artist\n");
+        printf("  -S  : Album Sort\n");
+        printf("  -O  : Performer Sort\n");
+        printf("  -T  : Title Sort\n\n");
+
         printf("EXAMPLES:\n");
         printf("  ./a.out -v song.mp3\n");
         printf("  ./a.out -e -t \"New Title\" song.mp3\n\n");
@@ -46,14 +59,18 @@ int main(int argc, char *argv[])
         return 0;
 	}
 
+
 	// id3 tag options
-	char *id3_tags[6][2] = 
+	char *id3_tags[24][2] = 
 	{
-		{"-t", "TIT2"}, {"-a", "TPE1"}, {"-A", "TALB"}, {"-y", "TYER"}, {"-c", "COMM"}, {"-C", "TCON"}
+		{"-t", "TIT2"},  {"-a", "TPE1"}, {"-b", "TPE2"}, {"-d", "TPE3"}, {"-r", "TPE4"}, {"-A", "TALB"},   
+        {"-n", "TRCK"},  {"-p", "TPOS"}, {"-g", "TCON"}, {"-y", "TYER"}, {"-D", "TDRC"}, {"-l", "TLAN"},    
+        {"-c", "COMM"},  {"-x", "TXXX"}, {"-w", "TEXT"}, {"-m", "TCOM"}, {"-C", "TCOP"}, {"-P", "TPUB"},    
+        {"-s", "TSRC"},  {"-L", "USLT"}, {"-o", "TOPE"}, {"-S", "TSOA"}, {"-O", "TSOP"}, {"-T", "TSOT"},    
 	};
 
-	
-	if ((strcmp(argv[1], "-v") == 0) && argc == 3) // view option
+
+	if (strcmp(argv[1], "-v") == 0) // view option
 	{
 		int last = argc - 1; // last argument is file name
 		char *filename = argv[last]; // file name
@@ -61,17 +78,8 @@ int main(int argc, char *argv[])
 	}
 	else if (strcmp(argv[1], "-e") == 0) // edit option
 	{
-		if (argc < 5) // must have at least 5 arguments
-		{
-			printf("-------------------------------------------------------------\n");
-			printf("  Invalid usage for edit option!\n");
-			printf("  Correct format: ./a.out -e <tag_option> <new_value> <file_name>\n");
-			printf("-------------------------------------------------------------\n");
-			return 0;
-		}
-
 		int j, flag = 0;
-		for(j = 0 ; j < 6 ; j++)
+		for(j = 0 ; j < 24 ; j++)
 		{
 			if(strcmp(argv[2], id3_tags[j][0]) == 0) // checking valid tag option
 			{
@@ -83,7 +91,7 @@ int main(int argc, char *argv[])
 		if(flag == 0) // invalid tag option
 		{
 			printf("-------------------------------------------------------------\n");
-            printf("  Invalid tag option \n");
+            printf("  Invalid tag option: %s\n", argv[2]);
             printf("  Use: ./a.out --help  to view all valid tag options.\n");
             printf("-------------------------------------------------------------\n");
             return 0;
@@ -108,9 +116,9 @@ int main(int argc, char *argv[])
 	else // invalid arguments
 	{
 		printf("-------------------------------------------------------------\n");
-        printf("  Invalid tag option \n");
-        printf("  Use: ./a.out --help  to view all valid tag options.\n");
-        printf("-------------------------------------------------------------\n");
+            printf("  Invalid tag option: %s\n", argv[2]);
+            printf("  Use: ./a.out --help  to view all valid tag options.\n");
+            printf("-------------------------------------------------------------\n");
 	}
 
 
